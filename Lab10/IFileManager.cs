964b728a047028b3f5663276fdd0f3d2971fbeb6
likaa08 <14angel_v14@mail.ru>
2026-05-10 22:25:53 +0500

    Update IFileManager.cs

--- Lab10/IFileManager.cs
+++ Lab10/IFileManager.cs
@@ -1,1 +1,13 @@
+namespace Lab10;
 
+public interface IFileManager
+{
+    string FolderPath { get; }
+    string FileName { get; }
+    string FileExtension { get; }
+    string FullPath { get; }
+
+    void SelectFolder(string folderPath);
+    void ChangeFileName(string newFileName);
+    void ChangeFileFormat(string newFileFormat);
+}
